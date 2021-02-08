205de6312c487075d3aa2fd875422293e3383b45
hayrapetyanstyle <hayrapetyanstyle@gmail.com>
2021-02-08 18:06:08 +0400

    project day 6

--- project/js/script.js
+++ project/js/script.js
@@ -2,83 +2,98 @@ window.addEventListener('load', () => {
     const mobileMenuBtn = document.querySelector('#mobile_menu_btn');
     const navMenu = document.querySelector('#nav_menu');
 
-    const offerModalBtn = document.querySelectorAll('[data-modal-btn]');
-    const offerModal = document.querySelector('[data-offer-modal]');
-    const offerModalClose = document.querySelector('[data-offer-close]');
-    const offerPrice = document.querySelector('[data-offerPrice]');
-    const offerModalSubmit = document.querySelector('[data-modal-submit]');
-    const offerModalH3 = document.querySelector('[data-offer-modal] h3');
-
-    const offerItem = document.querySelectorAll('.offer_item h3');
-
-    const countModalBtn = document.querySelector('.member_count');
-    const countModal = document.querySelector('[data-memberCount-modal]');
-    const countModalClose = document.querySelector('[data-count-close]')
-
-    const minusBtn = document.querySelectorAll('[data-minus]');
-    const plusBtn = document.querySelectorAll('[data-plus]');
-    const counterResault = document.querySelectorAll('[data-resault]');
+    const silver = `57.844 AMD`;
+    const gold = `98.455 AMD`;
+    const vip = `250.485 AMD`;
 
     mobileMenuBtn.addEventListener('click', () => {
         navMenu.classList.toggle('menu_active');
     });
 
-    offerModalBtn.forEach((btn, i) => {
-        btn.addEventListener('click', () => {
-            offerModal.style.display = 'flex';
-            let silver = `57.844 AMD`;
-            let gold = `98.455 AMD`;
-            let vip = `250.485 AMD`;
+    const modalPreview = (btn, modal, close) => {
+        btn = document.querySelector(btn);
+        close = document.querySelector(close);
+        modal = document.querySelector(modal);
 
-            if (i == 0) {
-                offerModalH3.innerHTML = `Վճարել։ ${offerItem[0].innerHTML}ի համար`;
-                offerPrice.innerHTML = `Վճարման ենթակա գումարը։ <span> ${silver}</span>`;
-                offerModalSubmit.setAttribute('value', `Վճարել ${silver}`)
-            }
-            if (i == 1) {
-                offerModalH3.innerHTML = `Վճարել։ ${offerItem[1].innerHTML}ի համար`;
-                offerPrice.innerHTML = `Վճարման ենթակա գումարը։ <span> ${gold}</span>`;
-                offerModalSubmit.setAttribute('value', `Վճարել ${gold}`)
-            }
-            if (i == 2) {
-                offerModalH3.innerHTML = `Վճարել։ ${offerItem[2].innerHTML}ի համար`;
-                offerPrice.innerHTML = `Վճարման ենթակա գումարը։ <span> ${vip}</span>`;
-                offerModalSubmit.setAttribute('value', `Վճարել ${vip}`)
-            }
-        });
-    });
-
-    offerModalClose.addEventListener('click', () => {
-        offerModal.style.display = 'none';
-    });
-
-    countModalBtn.addEventListener('click', () => {
-        countModal.style.display = 'flex';
-    });
-
-    countModalClose.addEventListener('click', () => {
-        countModal.style.display = 'none';
-    });
-
-    const counterFilter = (minus, plus, resault) => {
-        let i = 0;
-
-        minus.addEventListener('click', () => {
-            if (resault.innerHTML >= 1) {
-                resault.innerHTML = --i;
-            }
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            modal.style.display = 'flex';
         });
 
-        plus.addEventListener('click', () => {
-            if (resault.innerHTML <= 8) {
-                resault.innerHTML = ++i;
-            }
+        close.addEventListener('click', () => {
+            modal.style.display = 'none';
         });
     };
 
-    counterFilter(minusBtn[0], plusBtn[0], counterResault[0]);
-    counterFilter(minusBtn[1], plusBtn[1], counterResault[1]);
-    counterFilter(minusBtn[2], plusBtn[2], counterResault[2]);
+    modalPreview('#logBtn', '[data-loginOrReg-modal]', '[data-close-logmodal]');
+    modalPreview('.member_count', '[data-memberCount-modal]', '[data-count-close]');
+
+    const offerModalContent = (price, submit, title, money, offerItemh3) => {
+        title.innerHTML = `Վճարել։ ${offerItemh3.innerHTML}ի համար`;
+        price.innerHTML = `Վճարման ենթակա գումարը։ <span> ${money}</span>`;
+        submit.setAttribute('value', `Վճարել ${money}`)
+    };
+
+    const offerModalPreview = (button, modal, close, price, submit, title, offerItem) => {
+        button = document.querySelectorAll(button);
+        modal = document.querySelector(modal);
+        close = document.querySelector(close);
+        price = document.querySelector(price);
+        submit = document.querySelector(submit);
+        title = document.querySelector(title);
+        offerItem = document.querySelectorAll(offerItem);
+
+        button.forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                modal.style.display = 'flex';
+    
+                if (i == 0) {
+                    offerModalContent(price, submit, title, silver, offerItem[0]);
+                }
+                if (i == 1) {
+                    offerModalContent(price, submit, title, gold, offerItem[1]);
+                }
+                if (i == 2) {
+                    offerModalContent(price, submit, title, vip, offerItem[2]);
+                }
+            });
+        });
+
+        close.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+    }
+
+    offerModalPreview('[data-modal-btn]', '[data-offer-modal]', '[data-offer-close]', '[data-offerPrice]', '[data-modal-submit]', '[data-offer-modal] h3', '.offer_item h3');
+
+    const res = document.querySelectorAll('[data-resault]');
+    const minusBtn = document.querySelectorAll('[data-minus]');
+    const plusBtn = document.querySelectorAll('[data-plus]');
+
+    const counterFilter = (plus, minus, res) => {
+        let i = 0;
+
+        plus.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            if (res.innerHTML <= 8) {
+                res.innerHTML = ++i;
+            }
+        });
+
+        minus.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            if (res.innerHTML >= 1) {
+                res.innerHTML = --i;
+            }
+        });
+
+    };
+
+    counterFilter(plusBtn[0], minusBtn[0], res[0]);
+    counterFilter(plusBtn[1], minusBtn[1], res[1]);
+    counterFilter(plusBtn[2], minusBtn[2], res[2]);
 
     const swiper = new Swiper('.swiper-container', {
         slidesPerView: 5,
@@ -123,7 +138,4 @@ window.addEventListener('load', () => {
             swiper.autoplay.start()
         });
     })
-
-    console.dir(swiperSlide);
-
 });
